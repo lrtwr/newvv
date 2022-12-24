0b3648df879a5239c1134e3fcb6f13b78e00ece1
lrtwr <jeroen.leertouwer@outlook.com>
2022-12-24 13:10:48 +0100

    1 file modified

--- index.tsx
+++ index.tsx
@@ -7,9 +7,10 @@ import App from './App';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
+
 root.render(
   <StrictMode>
     <App />
-    
+
   </StrictMode>
 );
